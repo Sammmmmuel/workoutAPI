be5d5e634ff3e8d8ce19386b73d4b9873f8c44c6
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-03-05 20:42:03 -0800

    added gitignore

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
-const app = require("./../server");
+const app = require("./../app");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe("Server Root", () => {
+describe("Working server", () => {
     it("should return a success message", async() => {
         expect(true).to.be.equal(true)
         const res = await chai.request(app).get("")
-        expect(res.text).to.equal("API is working")
+        expect(res.text).to.equal("API is working!")
     })
 })
