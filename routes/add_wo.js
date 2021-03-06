bab0e8a98199689f3da5bcbd813fa426e4265625
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-03-05 17:54:23 -0800

    added delete post route

--- routes/add_wo.js
+++ routes/add_wo.js
@@ -2,10 +2,20 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/AddWo')
 
-router.get('/', (req, res) => {
-    res.send('we are on posts')
+
+
+// get back all posts
+router.get('/', async(req, res) => {
+    try {
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (err) {
+        res.json({ message: err })
+    }
 })
 
+
+// submits a post
 router.post('/', (req, res) => {
     const post = new Post({
         name: req.body.name,
@@ -21,5 +31,24 @@ router.post('/', (req, res) => {
         })
 })
 
+// gets specific post
+router.get('/:postId', async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId)
+        res.json(post)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+// delete post 
+router.delete('/:postId', async(req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params, postId })
+        res.json(removedPost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
 
 module.exports = router
